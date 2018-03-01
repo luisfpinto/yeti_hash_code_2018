a4cc2a5a98e3210c3d6d3249058786fc7caa48bd
luisfpinto <luisfpintos@gmail.com>
2018-03-01 20:27:26 +0100

    getRideCost

--- src/getRideCost.js
+++ src/getRideCost.js
@@ -1,10 +1,13 @@
-export function getRideCost (distanceTrip1,distanceTrip2, earlyStart1, earlyStart2) {
-  let totalCostTrip1 = distanceTrip1 + earlyStart1
-  let totalCosTrip
-  if (totalCostTrip1 < earlyStart2) {
-    totalCosTrip = totalCostTrip1 + (earlyStart2 - totalCostTrip1)
-  } else {
-    totalCosTrip = totalCostTrip1 + distanceTrip2
+var calcTimeBetween = require('./calcTimeBetween')
+
+export function getRideCost (currentTime, currPosition, ride) {
+  let timeToWait = 0
+  // Ver distancia de un punto donde estoy al inicio del siguiente
+  let distanceToInitial = calcTimeBetween(currPosition, [ride.a, ride.b])
+  // Ver si tengo que esperar para empezar el siguiente viaje
+  if (ride.s - (currentTime + distanceToInitial) > 0) {
+    timeToWait = ride.s - currentTime + distanceToInitial
   }
-  return totalCosTrip
+  // Total de ir a recoger al nota y hacer el viaje
+  return distanceToInitial + timeToWait + calcTimeBetween([ride.a, ride.b], [ride.x, ride.y])
 }
