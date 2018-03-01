4efd4a1ae532dc77296f1e1c53f3d5f6ce3e3e66
franrios <fcojriosbello@gmail.com>
2018-03-01 20:46:07 +0100

    Adds outputs naive

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const fs = require('fs')
 const processInput = require('./processInput')
 
-
+// For each file in inputs direcory
 fs.readdirSync('./inputs').forEach(file => {
   const input = processInput(`./inputs/${file}`)
-  console.log(input)
+  const {F} = input
+  let output = ''
+  console.log(typeof(F))
+  for (let i = 0; i < F; i++) {
+    output.concat(`${i + 1} ${i}\n`)
+    // fs.appendFile(`outputs/output_${file}`, `${i + 1} ${i}\n`, function(err) {
+    //   if (err) return console.log(err)
+    //   console.log("Appended!")
+    // })
+  }
 })
+
+
+// const fs = require("fs")
+// const processInput = require("./processInput")
+
+// // For each file in inputs direcory
+// fs.readdirSync("./inputs").forEach(file => {
+//   const input = processInput(`./inputs/${file}`)
+//   const { F } = input
+//   console.log(F, file)
+//   for (let i = F; i < F; i++) {
+//     fs.appendFile(`outputs/output_${file}`, `siii`, function(err) {
+//       if (err) return console.log(err)
+//       console.log("Appended!")
+//     })
+//   }
+// })
 
 
