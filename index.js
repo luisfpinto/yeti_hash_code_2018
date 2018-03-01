a5a2925eff382843b61632ed74b9762c5ed1187d
franrios <fcojriosbello@gmail.com>
2018-03-01 18:23:26 +0100

    Updated with simple parser

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const parser = require("jolicitron")
+const fs = require("fs")
 
-const parser = require('jolicitron')
+const file = fs.readFileSync("sample.in", "utf8")
+console.log(file)
+
+const parsed = jolicitron((save, n) => [
+  save("Ori"),
+  save("Dest"),
+  save("i"),
+  n("paths", { length: "i" })
+])
+
+console.log(parsed(file))
