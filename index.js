74c95b8f52a3e79f9eccafc018cb3b5cbc0ce2c7
jsdario <jesus@netbeast.co>
2018-03-01 22:26:19 +0100

    Final working pice

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const processInput = require('./src/processInput')
 // const {runSimulation} = require('./src/getBestSimulation')
 
 // For each file in inputs direcory
-const file = 'a_example.in'
-// fs.readdirSync('./inputs').forEach(file => {
-const input = processInput(`./inputs/${file}`)
-const solution = getBestSimulation(input)
-console.log(solution)
-const output = solution.reduce((acc, vehicleServices) => {
-  const servicesString = vehicleServices.map(service => ' ' + service.id).join('')
-  return acc + `${vehicleServices.length}${servicesString}\n`
-}, '')
-fs.writeFileSync(`outputs/output_${file}`, output)
-console.log('Finished with ', `outputs/output_${file}`)
-// })
+// const file = 'a_example.in'
+fs.readdirSync('./inputs').forEach(file => {
+  const input = processInput(`./inputs/${file}`)
+  const solution = getBestSimulation(input)
+  console.log(solution)
+  const output = solution.reduce((acc, vehicleServices) => {
+    const servicesString = vehicleServices.map(service => ' ' + service.id).join('')
+    return acc + `${vehicleServices.length}${servicesString}\n`
+  }, '')
+  fs.writeFileSync(`outputs/output_${file}`, output)
+  console.log('Finished with ', `outputs/output_${file}`)
+})
